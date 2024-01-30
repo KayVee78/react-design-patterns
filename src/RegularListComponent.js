05109918821a6737a7a156e376a3b2dcfc9de45d
Kithmi Hettiarachchi <kithmi@xgengroup.com.au>
2024-01-30 20:29:06 +0530

    List Item Component

---
+++ src/RegularListComponent.js
@@ -0,0 +1,15 @@
+export const RegularListComponent = ({
+  items,
+  resourceName,
+  itemComponent: ItemComponent,
+}) => {
+  return (
+    <>
+      {items.map((item, i) => (
+        <ItemComponent key={i} {...{ [resourceName]: item }} />
+      ))}
+    </>
+  );
+};
+
+//{...{ [resourceName]: item }}    means ==>    person={item}
---
+++ src/people/SmallPersonListItem.js
@@ -0,0 +1,10 @@
+export const SmallPersonListItem = ({ person }) => {
+    const { name, age } = person;
+  
+    return (
+      <p>
+        Name: {name}, Age: {age} years
+      </p>
+    );
+  };
+  
