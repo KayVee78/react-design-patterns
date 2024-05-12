015d965103266c2e184a7495eb7c924508a5b6b4
Kithmi Hettiarachchi <kithmi@xgengroup.com.au>
2024-05-12 09:58:03 +0530

    Data source component to load data without knowing the resource, and to load data from local storage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import { UserInfo } from "./UserInfo";
-import { ResourceLoader } from "./ResourceLoader";
-import { ProductInfo } from "./ProductInfo";
+import axios from "axios";
+import { DataSource } from "./DataSource";
+
+const getServerData = (url) => async () => {
+  const response = await axios.get(url);
+  return response.data;
+};
+
+const getLocalStorageData = (key) => () => {
+  return localStorage.getItem(key);
+};
+
+const Text = ({ message }) => {
+  return <h1>{message}</h1>;
+};
 
 function App() {
   return (
     <>
-      <ResourceLoader resourceUrl={"/users/123"} resourceName={"user"}>
+      <DataSource getDataFunc={getServerData("/users/123")} resourceName="user">
         <UserInfo />
-      </ResourceLoader>
-      <ResourceLoader resourceUrl={"/products/1234"} resourceName={"product"}>
-        <ProductInfo />
-      </ResourceLoader>
+      </DataSource>
+
+      <DataSource
+        getDataFunc={getLocalStorageData("message")}
+        resourceName="message"
+      >
+        <Text />
+      </DataSource>
     </>
   );
 }
