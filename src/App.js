48f819911845f450adfc1e1b18817e98014100bd
Kithmi Hettiarachchi <kithmi@xgengroup.com.au>
2024-05-17 10:22:25 +0530

    UserInfo Form to modify data in the server using HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { withUser } from "./withUser";
 import { UserInfo } from "./UserInfo";
+import { UserInfoForm } from "./UserInfoForm";
 
 const UserInfoWithLoader = withUser(UserInfo, "234");
 
 function App() {
-  return <UserInfoWithLoader />;
+  // return <UserInfoWithLoader />;
+  return <UserInfoForm />;
 }
 
 export default App;
