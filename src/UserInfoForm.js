5f8d44241aee07d39e53a8f6e6339d1f78860bdb
Kithmi Hettiarachchi <kithmi@xgengroup.com.au>
2024-05-17 10:48:58 +0530

    created generic HOC withEditableResource

--- src/UserInfoForm.js
+++ src/UserInfoForm.js
@@ -1,7 +1,43 @@
-import { withEditableUser } from "./withEditableUser";
+// import { withEditableUser } from "./withEditableUser";
+import { withEditableResource } from "./withEditableResource";
 
-//Using HOC within a regular component
-export const UserInfoForm = withEditableUser(
+//Using withEditableUser HOC within a regular component
+// export const UserInfoForm = withEditableUser(
+//   ({ user, onChangeUser, onSaveUser, onResetUser }) => {
+//     const { name, age, hairColor } = user || {};
+
+//     return user ? (
+//       <>
+//         <label>Name:</label>
+//         <input
+//           type="text"
+//           value={name}
+//           onChange={(e) => onChangeUser({ name: e.target.value })}
+//         />
+//         <label>Age:</label>
+//         <input
+//           type="number"
+//           value={age}
+//           onChange={(e) => onChangeUser({ age: Number(e.target.value) })}
+//         />
+//         <label>Hair Color:</label>
+//         <input
+//           type="text"
+//           value={hairColor}
+//           onChange={(e) => onChangeUser({ hairColor: e.target.value })}
+//         />
+//         <button onClick={onResetUser}>Reset</button>
+//         <button onClick={onSaveUser}>Save Changes</button>
+//       </>
+//     ) : (
+//       <p>Loading...</p>
+//     );
+//   },
+//   "123"
+// );
+
+//Using withEditableResource HOC within a regular component
+export const UserInfoForm = withEditableResource(
   ({ user, onChangeUser, onSaveUser, onResetUser }) => {
     const { name, age, hairColor } = user || {};
 
@@ -32,5 +68,6 @@ export const UserInfoForm = withEditableUser(
       <p>Loading...</p>
     );
   },
-  "123"
+  "/users/123",
+  "user"
 );
---
+++ src/withEditableResource.js
@@ -0,0 +1,46 @@
+// withEditableResource (same functionality as of withEditableUser) but more generic and reusable
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+//Capitalizes the first letter of a string
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+export const withEditableResource = (Component, resourcePath, resourceName) => {
+  return (props) => {
+    const [originalData, setOriginalData] = useState(null); //represent the original data that on the server side
+    const [data, setData] = useState(null); //current data that we're editing  on the client side
+
+    useEffect(() => {
+      (async () => {
+        const response = await axios.get(resourcePath);
+        setOriginalData(response.data);
+        setData(response.data);
+      })();
+    }, []);
+
+    const onChange = (changes) => {
+      setData({ ...data, ...changes });
+    };
+
+    const onSave = async () => {
+      const response = await axios.post(resourcePath, { [resourceName]: data });
+      setOriginalData(response.data);
+      setData(response.data);
+    };
+
+    //Reset all changes and set original data which has when we first load data from the server
+    const onReset = () => {
+      setData(originalData);
+    };
+
+    const resourceProps = {
+      [resourceName]: data,
+      [`onChange${capitalize(resourceName)}`]: onChange,
+      [`onSave${capitalize(resourceName)}`]: onSave,
+      [`onReset${capitalize(resourceName)}`]: onReset,
+    };
+
+    return <Component {...props} {...resourceProps} />;
+  };
+};
