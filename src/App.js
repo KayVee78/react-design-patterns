876bb8eadbe404ed74212db2efee2d10e505a41c
Kithmi Hettiarachchi <kithmi@xgengroup.com.au>
2024-05-17 09:42:41 +0530

    Loading data with HOCs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import { withUser } from "./withUser";
 import { UserInfo } from "./UserInfo";
-import { printProps } from "./printProps";
 
-//How we are gonna use printProps is by creating a wrapper around UserInfo component
-const UserInfoWrapped = printProps(UserInfo);
+const UserInfoWithLoader = withUser(UserInfo, "234");
 
 function App() {
-  return <UserInfoWrapped a={1} b="Hello" c={{ name: "Shaun" }} />;
+  return <UserInfoWithLoader />;
 }
 
 export default App;
