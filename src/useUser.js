2454dae44f4fc5f076b56133ff6d864ec50b1a85
Kithmi Hettiarachchi <kithmi@xgengroup.com.au>
2024-05-17 20:53:25 +0530

    Created useUser hook - to load any user from the server

---
+++ src/useUser.js
@@ -0,0 +1,17 @@
+//Custom hook to load any user
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export const useUser = (userId) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get(`/users/${userId}`);
+      setUser(response.data);
+    })();
+  }, [userId]);
+
+  return user;
+};
