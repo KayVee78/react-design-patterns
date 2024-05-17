103e995052db8ca833dca8c483a570f54e9ee1b9
Kithmi Hettiarachchi <kithmi@xgengroup.com.au>
2024-05-17 20:48:24 +0530

    Created useCurrentUser hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import { withUser } from "./withUser";
 import { UserInfo } from "./UserInfo";
-import { UserInfoForm } from "./UserInfoForm";
-
-const UserInfoWithLoader = withUser(UserInfo, "234");
 
 function App() {
-  // return <UserInfoWithLoader />;
-  return <UserInfoForm />;
+  return <UserInfo />;
 }
 
 export default App;
